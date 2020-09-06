f3822b9da2a87cdf099e04af97b8755e3bce7c3c
Fabrice Latri <fabrice.latri@hotmail.fr>
2020-09-06 18:40:19 +0200

    added Router

--- public/index.php
+++ public/index.php
@@ -1,29 +1,47 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
-require __DIR__ . '/../vendor/autoload.php';
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 $request = Request::createFromGlobals();
 
 $response = new Response();
 
-$map = [
-    '/hello' => 'hello.php',
-    '/bye' => 'bye.php',
-    '/a-propos' => 'about.php'
-];
+$routes = new RouteCollection();
+
+$routes->add('hello', new Route('/hello/{name}', ['name' => 'World']));
+$routes->add('bye', new Route('/bye'));
+$routes->add('cms/about', new Route('/a-propos'));
+
+$context = new RequestContext();
+
+$context->fromRequest($request);
+
+$urlMatcher = new UrlMatcher($routes, $context);
 
 $pathInfo = $request->getPathInfo();
 
-if (isset($map[$pathInfo])) {
+try {
+    $attributes = $urlMatcher->match($pathInfo);
+    extract($attributes);
+    extract($request->query->all());
+
     ob_start();
-    include __DIR__ . '/../src/pages/' . $map[$pathInfo];
+    include __DIR__ . '/../src/pages/' . $attributes['_route'] . '.php';
     $response->setContent(ob_get_clean());
-} else {
-    $response->setContent('La page demandée n\'existe pas');
-    $response->setStatusCode(404);
+
+} catch (ResourceNotFoundException $exception) {
+        $response->setContent('La page demandée n\'existe pas');
+        $response->setStatusCode(404);
+} catch (Exception $exception) {
+    $response = new Response('An error occurred', 500);
 }
 
 $response->send();
--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,7 +1,11 @@
-<?php
-
-$name = $request->query->get('name', 'World');
-
-?>
-Hello <?= htmlspecialchars($name, ENT_QUOTES) ?>
-
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Symfony Framework</title>
+</head>
+<body>
+    <h1>Hello <?=htmlspecialchars(isset($name) ? $name : 'World', ENT_QUOTES)?></h1>
+</body>
+</html>
