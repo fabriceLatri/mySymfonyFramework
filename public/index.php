dc634b15bd9da5f907ebb32abbbb589a60a56522
Fabrice Latri <fabrice.latri@hotmail.fr>
2020-10-01 19:22:40 +0200

    added Framework class and phpunit.xml.dist

--- public/index.php
+++ public/index.php
@@ -2,14 +2,12 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Symfony\Component\Routing\Route;
+use App\Framework\Kernel;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 $request = Request::createFromGlobals();
 
@@ -26,27 +24,7 @@ $controllerResolver = new ControllerResolver();
 // ArgumentResolver permet d'envoyer les arguments demandés par la méthode appelée.
 $argumentResolver = new ArgumentResolver();
 
-
-try {
-    // Recherche la route depuis l'url donnée.
-    $request->attributes->add($urlMatcher->match($request->getPathInfo()));
-
-    // Permet de retrouver le controller depuis routes.php grâce à la clé '_controller'. Attention, la value entre l'instance et la méthode doit être séparée par ::
-    $controller = $controllerResolver->getController($request);
-
-    /*  Permet de récupérer les arguments depuis la request->attributes (grâce à l'URL Matcher):
-
-        Récupère la valeur de {name} en pour l'injecter par réflexion à la méthode du controller invoquée.
-    */
-    $arguments = $argumentResolver->getArguments($request, $controller);
-
-    // call_user_func permet d'invoquer la méthode d'un objet par le système de callable [Nom de l'instance, Nom de la méthode].
-    $response = call_user_func_array($controller, $arguments);
-
-} catch (ResourceNotFoundException $exception) {
-    $response = new Response('La page demandée n\'existe pas', 404);
-} catch (Exception $exception) {
-    $response = new Response('An error occurred', 500);
-}
+$framework = new Kernel($urlMatcher, $controllerResolver, $argumentResolver);
+$response = $framework->handle($request);
 
 $response->send();
---
+++ src/Framework/Kernel.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Framework;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+
+class Kernel
+{
+    protected $matcher;
+    protected $controllerResolver;
+    protected $argumentResolver;
+
+    public function __construct(UrlMatcher $matcher, ControllerResolver $controllerResolver, ArgumentResolver $argumentResolver)
+    {
+        $this->matcher = $matcher;
+        $this->controllerResolver = $controllerResolver;
+        $this->argumentResolver = $argumentResolver;
+    }
+
+    public function handle(Request $request)
+    {
+        // Récupération des elements de la requête selon le context défini.
+        $this->matcher->getContext()->fromRequest($request);
+
+        try {
+            // Recherche la route depuis l'url donnée.
+            $request->attributes->add($this->matcher->match($request->getPathInfo()));
+
+            // Permet de résoudre le controller à instancier depuis routes.php grâce à la clé '_controller'.
+            $controller = $this->controllerResolver->getController($request);
+
+            /*  Permet de récupérer les arguments depuis la request->attributes (grâce à l'URL Matcher):
+
+                Récupère la valeur de {argument} en pour l'injecter par réflexion à la méthode du controller invoquée.
+            */
+            $arguments =$this->argumentResolver->getArguments($request, $controller);
+
+            return call_user_func_array($controller, $arguments);
+        } catch (ResourceNotFoundException $exception) {
+            return new Response('La page demandée n\'existe pas', 404);
+        } catch (\Exception $exception) {
+            return new Response('An error occured', 500);
+        }
+    }
+}
